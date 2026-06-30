427d8d066a035fa802e0863da700fac9fb99271b
Josiah Smith <jcsmithf22@gmail.com>
2026-06-30 10:28:34 -0500

    add option for smear

--- neovim/.config/nvim/lua/custom/plugins/smear.lua
+++ neovim/.config/nvim/lua/custom/plugins/smear.lua
@@ -1,18 +1,20 @@
-vim.pack.add {
-  'https://github.com/sphamba/smear-cursor.nvim',
-}
+if vim.g.smear then
+  vim.pack.add {
+    'https://github.com/sphamba/smear-cursor.nvim',
+  }
 
-require('smear_cursor').setup {
-  -- Your options
-  cursor_color = "#868D96"
-  -- legacy_computing_symbols_support = true,
+  require('smear_cursor').setup {
+    -- Your options
+    cursor_color = '#868D96',
+    -- legacy_computing_symbols_support = true,
 
-  -- cursor_color = '#ff4000',
-  -- particles_enabled = true,
-  -- particle_max_num = 200,
-  -- stiffness = 0.5,
-  -- trailing_stiffness = 0.2,
-  -- trailing_exponent = 5,
-  -- damping = 0.6,
-  -- gradient_exponent = 0,
-}
+    -- cursor_color = '#ff4000',
+    -- particles_enabled = true,
+    -- particle_max_num = 200,
+    -- stiffness = 0.5,
+    -- trailing_stiffness = 0.2,
+    -- trailing_exponent = 5,
+    -- damping = 0.6,
+    -- gradient_exponent = 0,
+  }
+end
