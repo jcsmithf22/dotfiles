e24c5e041382e0425ce5d24a6e5252183612194d
Josiah Smith <jcsmithf22@gmail.com>
2026-06-30 10:28:34 -0500

    add option for smear

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -101,6 +101,8 @@ do
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = true
 
+  vim.g.smear = false
+
   -- [[ Setting options ]]
   --  See `:help vim.o`
   -- NOTE: You can change these options as you wish!
